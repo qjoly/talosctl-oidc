80f0154cc2f3a9c44ce77ccc7881c4fbfe28f935
Quentin JOLY <quentinj@une-pause-cafe.fr>
2026-02-19 17:39:16 +0100

    Add talosctl-oidc version (#60)

--- main.go
+++ main.go
@@ -6,7 +6,15 @@ import (
 	"github.com/qjoly/talosctl-oidc/cmd"
 )
 
+// version, commit and date are set at build time via -ldflags.
+var (
+	version string
+	commit  string
+	date    string
+)
+
 func main() {
+	cmd.SetBuildInfo(version, commit, date)
 	if err := cmd.Execute(); err != nil {
 		os.Exit(1)
 	}
